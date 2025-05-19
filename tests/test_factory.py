7b6566c4d502edd87d96590a3e8aabcb5918debe
Thiago Gomez <thiagogomez12300@gmail.com>
2025-05-19 11:42:09 -0300

    Agrega pruebas unitarias con pytest y coverage para toda la aplicación Flask

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -9,3 +9,15 @@ def test_config():
 def test_hello(client):
     response = client.get('/hello')
     assert response.data == b'Hello, World!'
+
+from flaskr import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
